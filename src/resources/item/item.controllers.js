6a90903c6ac9b80eb9f85c8011dc20020a637248
moremichelle <michelle.more@promart.pe>
2020-01-16 09:51:34 -0500

    Excercise done

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,1 +1,17 @@
-export default {}
+export default {
+  getOne: (req, res) => {
+    res.send({ message: 'hello' })
+  },
+  getMany: (req, res) => {
+    res.send({ message: 'hello' })
+  },
+  createOne: (req, res) => {
+    res.send({ message: 'hello' })
+  },
+  removeOne: (req, res) => {
+    res.send({ message: 'hello' })
+  },
+  updateOne: (req, res) => {
+    res.send({ message: 'hello' })
+  }
+}
--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,26 @@
 import { Router } from 'express'
+// import {
+//   getOne,
+//   getMany,
+//   createOne,
+//   removeOne,
+//   updateOne
+// } from './item.controllers'
+import controllers from './item.controllers'
 
 const router = Router()
+
+// api/item
+router
+  .route('/')
+  .get(controllers.getMany)
+  .post(controllers.createOne)
+
+// api/item/:id
+router
+  .route('/:id')
+  .get(controllers.getOne)
+  .delete(controllers.removeOne)
+  .put(controllers.updateOne)
 
 export default router
