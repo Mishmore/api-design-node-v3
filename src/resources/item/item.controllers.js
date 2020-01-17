744602f97cfa6216004a1535a5a906edfee67e8b
moremichelle <michelle.more@promart.pe>
2020-01-17 15:18:28 -0500

    Got error

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,1 +1,19 @@
-export default {}
+import { Item } from './item.model'
+import mongoose from 'mongoose'
+import { connect } from '../../utils/db'
+
+const run = async () => {
+  await connect('mongodb://localhost:27017/api-test')
+
+  const item = await Item.create({
+    name: 'Clean up',
+    createdBy: mongoose.Type.ObjectId,
+    list: mongoose.Type.ObjectId
+  })
+
+  console.log(await Item.findById(item._id).exec())
+}
+
+run()
+
+// export default {}
